ca3cd9746fe4a936397ed83ea0accf085f0da2a5
selimdilsad <selimdilsadercan@gmail.com>
2023-08-19 17:20:37 +0300

    dnemee

---
+++ sanity/schema.ts
@@ -0,0 +1,6 @@
+import { type SchemaTypeDefinition } from "sanity";
+import { project } from "./schemas/project";
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [project],
+};
---
+++ sanity/schemas/project.ts
@@ -0,0 +1,14 @@
+import { defineField, defineType } from "sanity";
+
+export const project = defineType({
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+  ],
+});
